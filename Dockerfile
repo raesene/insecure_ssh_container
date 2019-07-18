d1addff4ea18c2ac21bbf842909f64b81eb1b2c8
Rory McCune <rory.mccune@nccgroup.trust>
2019-07-18 18:58:47 +0100

    fix entrypoint

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt update && apt install -y openssh-server
 RUN groupadd -g 999 sshuser && useradd -r -u 999 -g sshuser sshuser
 RUN echo "sshuser:sshuser" | chpasswd
 RUN mkdir -p /home/sshuser/
+RUN wget https://storage.googleapis.com/kubernetes-release/release/v1.14.4/bin/linux/amd64/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin
 COPY entrypoint.sh /
 RUN chmod +x entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,20 @@ IPADDRESS="$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print 
 echo IP Address: $IPADDRESS
 }
 
+__set_env() {
+echo "export KUBERNETES_SERVICE_HOST=10.96.0.1" >> /etc/profile
+echo "export KUBERNETES_SERVICE_PORT=443" >> /etc/profile
+echo "export KUBERNETES_PORT_443_TCP_PROTO=tcp" >> /etc/profile
+echo "export KUBERNETES_PORT_443_TCP_ADDR=10.96.0.1" >> /etc/profile
+echo "export KUBERNETES_PORT=tcp://10.96.0.1:443" >> /etc/profile
+}
+
 
 # Call all functions
 __create_rundir
 __create_hostkeys
 __create_user
 __ip_address
+__set_env
 
 exec /usr/sbin/sshd -D -p 3456 -e "$@"
